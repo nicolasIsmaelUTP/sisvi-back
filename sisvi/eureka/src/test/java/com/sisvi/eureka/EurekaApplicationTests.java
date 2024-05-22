79bdced0075a91b9524e173a6f1807397913f9bb
Nicolas Alayo <n_alayo_arias@outlook.com>
2024-05-22 11:53:21 -0500

    Add Eureka module to sisvi project

---
+++ sisvi/eureka/src/test/java/com/sisvi/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisvi.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
