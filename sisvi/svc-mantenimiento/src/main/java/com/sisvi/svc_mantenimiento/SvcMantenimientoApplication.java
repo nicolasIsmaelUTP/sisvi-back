ce4f09dd65d2abe7e7272cb57e49c85e020508df
Nicolas Alayo <n_alayo_arias@outlook.com>
2024-05-23 13:45:57 -0500

    Add svc-mantenimiento module and related classes

---
+++ sisvi/svc-mantenimiento/src/main/java/com/sisvi/svc_mantenimiento/SvcMantenimientoApplication.java
@@ -0,0 +1,13 @@
+package com.sisvi.svc_mantenimiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvcMantenimientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvcMantenimientoApplication.class, args);
+	}
+
+}
---
+++ sisvi/svc-mantenimiento/src/test/java/com/sisvi/svc_mantenimiento/SvcMantenimientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisvi.svc_mantenimiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvcMantenimientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
