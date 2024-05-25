3833c023a892d24e96468b0488d39ca4cc9aa8ec
Nicolas Alayo <n_alayo_arias@outlook.com>
2024-05-24 22:52:29 -0500

    Add svc-parametros module to sisvi/pom.xml

---
+++ sisvi/svc-parametros/src/main/java/com/sisvi/svc_parametros/SvcParametrosApplication.java
@@ -0,0 +1,13 @@
+package com.sisvi.svc_parametros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvcParametrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvcParametrosApplication.class, args);
+	}
+
+}
