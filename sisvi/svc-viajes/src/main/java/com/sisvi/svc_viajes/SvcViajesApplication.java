b2fd86fbabfbe86f04c74f5dfe22ad37ce86eb23
Nicolas Alayo <n_alayo_arias@outlook.com>
2024-07-09 13:56:26 -0500

    Add svc-viajes module and configuration

---
+++ sisvi/svc-viajes/src/main/java/com/sisvi/svc_viajes/SvcViajesApplication.java
@@ -0,0 +1,13 @@
+package com.sisvi.svc_viajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvcViajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvcViajesApplication.class, args);
+	}
+
+}
---
+++ sisvi/svc-viajes/src/test/java/com/sisvi/svc_viajes/SvcViajesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisvi.svc_viajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvcViajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
