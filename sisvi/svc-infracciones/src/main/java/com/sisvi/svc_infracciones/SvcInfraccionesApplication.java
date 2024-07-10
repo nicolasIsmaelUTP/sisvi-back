803aa7aab9d1fe171b89b879cb93a55177edcf27
Nicolas Alayo <n_alayo_arias@outlook.com>
2024-07-09 19:09:40 -0500

    Add svc-infracciones module and configuration

---
+++ sisvi/svc-infracciones/src/main/java/com/sisvi/svc_infracciones/SvcInfraccionesApplication.java
@@ -0,0 +1,13 @@
+package com.sisvi.svc_infracciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvcInfraccionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvcInfraccionesApplication.class, args);
+	}
+
+}
---
+++ sisvi/svc-infracciones/src/test/java/com/sisvi/svc_infracciones/SvcInfraccionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisvi.svc_infracciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvcInfraccionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
