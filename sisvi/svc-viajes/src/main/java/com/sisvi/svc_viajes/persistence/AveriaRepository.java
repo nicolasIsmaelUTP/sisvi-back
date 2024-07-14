78a62be5d441cce4ace38e68784199e24a8383be
EduardoJBH <U21227383@utp.edu.pe>
2024-07-13 20:11:51 -0500

    Agregué la anotación Repository

--- sisvi/svc-viajes/src/main/java/com/sisvi/svc_viajes/persistence/AveriaRepository.java
+++ sisvi/svc-viajes/src/main/java/com/sisvi/svc_viajes/persistence/AveriaRepository.java
@@ -3,9 +3,11 @@ package com.sisvi.svc_viajes.persistence;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.sisvi.svc_viajes.entities.Averia;
 
+@Repository
 public interface AveriaRepository extends CrudRepository<Averia, Long> {
     List<Averia> findByVehiculoId(Long vehiculoId);
     List<Averia> findByEstado(Boolean estado);
