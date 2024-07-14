b4d39aa360755797a433da045c43db10637d2942
EduardoJBH <U21227383@utp.edu.pe>
2024-07-13 20:17:31 -0500

    Correcion de libreria

--- sisvi/svc-viajes/src/main/java/com/sisvi/svc_viajes/controller/AveriaController.java
+++ sisvi/svc-viajes/src/main/java/com/sisvi/svc_viajes/controller/AveriaController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +14,6 @@ import org.springframework.http.HttpStatus;
 
 import com.sisvi.svc_viajes.http.request.AveriaRequest;
 import com.sisvi.svc_viajes.service.PAveriaService;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api/averias")
